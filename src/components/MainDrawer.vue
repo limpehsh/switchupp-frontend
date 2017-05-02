7c0ae95856898ea4f287e933dc40bf8cf47aef7d
Mike <michlee@student.unimelb.edu.au>
2017-05-03 02:16:06 +1000

    Making code neater

--- src/components/MainDrawer.vue
+++ src/components/MainDrawer.vue
@@ -1,60 +1,77 @@
 <template>
   <q-drawer>
+
+    <!-- Header/Main Page Link -->
     <div class="toolbar bg-blue-9">
       <q-toolbar-title :padding="1">
           Sidebar
       </q-toolbar-title>
     </div>
+    <!-- List of fields -->
     <div class="list platform-delimiter">
+      <!-- header title not needed now I think -->
+      <!--
       <div class="list-header">
-        <!-- header title not needed now I think -->
+        Header
       </div>
+      -->
+
       <router-link to="/about-us">
         <div class="tabcolors">
           <q-drawer-link icon="sentiment_very_satisfied" to="/about-us">About Us</q-drawer-link>
         </div>
       </router-link>
+
       <router-link to="/feed">
         <div class="tabcolors">
           <q-drawer-link icon="timeline" to="/feed">Feed</q-drawer-link>
         </div>
       </router-link>
+
       <router-link to="/map">
         <div class="tabcolors">
           <q-drawer-link icon="map" to="/map">Map</q-drawer-link>
         </div>
       </router-link>
+
       <router-link to="/profile">
         <div class="tabcolors">
           <q-drawer-link icon="account_circle" to="/profile">Profile</q-drawer-link>
         </div>
       </router-link>
+
       <router-link to="/report">
         <div class="tabcolors">
           <q-drawer-link icon="report_problem" to="/report">Report</q-drawer-link>
         </div>
       </router-link>
+
       <router-link to="/settings">
         <div class="tabcolors">
           <q-drawer-link icon="settings" to="/settings">Settings</q-drawer-link>
         </div>
       </router-link>
+
       <router-link to="/sign-up">
         <div class="tabcolors">
           <q-drawer-link icon="play_for_work" to="/sign-up">Sign Up</q-drawer-link>
         </div>
       </router-link>
+
       <router-link to="/sign-in">
         <div class="tabcolors">
           <q-drawer-link icon="input" to="/sign-in">Sign In</q-drawer-link>
         </div>
       </router-link>
+
       <router-link to="/">
         <div class="tabcolors">
           <!-- Log Out parens placeholder -->
           <q-drawer-link icon="exit_to_app" to="/log-out">Log Out</q-drawer-link>
         </div>
       </router-link>
+
+      <!-- Testing Dialogs -->
       <div
         class="item item-link"
         v-for="dialog in form"
