ddedd2327022be73e4f9fe0b7f98374acb4887b9
Mike <michlee@student.unimelb.edu.au>
2017-05-03 03:46:42 +1000

    Very Simple test of conditional rendering in Report.vue
    Only things left that I'm not 100% sure at this stage in terms of functionality are referencing the GETs and DELETE, MODIFY stuff.

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -2,14 +2,34 @@
   <div>
     <div class="layout-padding">
       <!-- content for component -->
+      <h1 v-if="isLoggedIn">Logged In</h1>
+      <h1 v-else>Failed</h1>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 export default {
+  name: 'report',
   data () {
-    return {}
+    return {
+      isLoggedIn: false
+    }
+  },
+  methods: {
+    findUser () {
+      Vue.axios.get('http://localhost:3001/user/')
+        .then((response) => {
+          console.log(response.data)
+      })
+    }
+    /* throttledMethod: _.debounce(() => {
+      console.log('I only get fired once every two seconds, max!')
+    }, 2000) */
   }
 }
 </script>
