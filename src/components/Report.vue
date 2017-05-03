f78f11d5442595ed32bf793f61de8cc8f218748c
Mike <michlee@student.unimelb.edu.au>
2017-05-03 22:42:21 +1000

    Solve Report.vue merge conflict

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -24,8 +24,31 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
 export default {
-    /* throttledMethod: _.debounce(() => {
-  name: 'report',
+  data: () => ({
+    posts: [],
+    errors: []
+  }),
+
+  // Fetches posts when the component is created.
+  created () {
+    // http://jsonplaceholder.typicode.com/posts
+    axios.get('http://localhost:3001/user/mail/lol@lol.com')
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
   }
 }
 </script>
