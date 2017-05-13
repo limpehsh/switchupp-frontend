4849790b0b07b5963d21041195f4f31332496980
Mike <michlee@student.unimelb.edu.au>
2017-05-14 05:00:23 +1000

    Finalised Placeholder for the Feed since I'm unsure how to reference the variable I reference in Feed.vue via Axios into PostBox.vue

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -10,14 +10,32 @@
           <!-- commented out temporarily -->
           <!-- <PostBox v-for="post of posts" :key="post._id"/> -->
           <ul v-if="posts && posts.length">
-            <li v-for="post of posts">
-              <p><strong>{{post.title}}</strong></p>
-              <p><strong>Location:</strong> {{post.locname}}</p>
-              <img :src='post.image' />
-              <p><strong>Description:</strong> {{post.desc}}</p>
-              <p><strong>Vote Score:</strong> {{post.votescore}}</p>
-              <p><strong>Created by</strong> {{post.author}} at {{post.createdAt}}</p>
-            </li>
+            <div v-for="post of posts">
+              <div class="card bg-blue-grey-1">
+                <div class="item one-line">
+                  <img class="item-primary" :src="post.image">
+                  <div class="item-content">
+                    <div>{{post.author}}</div>
+                  </div>
+                </div>
+                <img class="bg-white" :src='post.image'>
+                <div class="card-content">
+                  {{post.desc}}
+                </div>
+                <div class="card-actions">
+                  <div class="text-primary">
+                    <i>arrow_upward</i> {{post.votescore}} votes
+                  </div>
+                  <!--<div class="text-primary">
+                    <i>mode_comment</i> 8 comments
+                  </div>-->
+                  <div class="auto"></div>
+                  <div class="text-grey-6">
+                    {{post.createdAt}}
+                  </div>
+                </div>
+              </div>
+            </div>
           </ul>
         </div>
         <!-- END -->
